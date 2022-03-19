208e05016fe676c03650c192a55c1cfb1a9e214c
Тила Кирилл <tilakirill98@gmail.com>
2022-03-19 11:43:50 +0200

    Initial Commit

---
+++ SwiftUIviews/SwiftUIviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIviewsApp.swift
+//  SwiftUIviews
+//
+//  Created by Кирилл Тила on 19.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
